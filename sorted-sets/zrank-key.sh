4244b25ac04df406ea91fed58ef49c14ddeb3042
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-12 17:15:55 +0530

    ZRANK & ZREVRANK commands

---
+++ sorted-sets/zrank-key.sh
@@ -0,0 +1,6 @@
+# ZRANK key member
+
+ZRANK name Alex # 1
+ZRANK name Andrew # 2
+ZRANK name Rob # 3
+ZRANK name Arthur # 4
---
+++ sorted-sets/zrevrank-key.sh
@@ -0,0 +1,6 @@
+# ZREVRANK key member
+
+ZREVRANK name Alex # 3
+ZREVRANK name Andrew # 2
+ZREVRANK name Rob # 1
+ZREVRANK name Arthur # 0
