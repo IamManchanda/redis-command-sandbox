b3f97549393e6fc91042376ba790a8dcc3bc7021
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-17 21:08:33 +0530

    GEODIST command

--- geo/geoadd-key.sh
+++ geo/geoadd-key.sh
@@ -1,3 +1,5 @@
 # GEOADD key longitude latitude member [longitude latitude member ...]
 
 GEOADD geopoints -120.544046 46.995344 Bank
+GEOADD geopoints -120.568864 47.042893 "Junction Road"
+GEOADD geopoints -120.474217 47.974739 Park
---
+++ geo/geodist-key.sh
@@ -0,0 +1,10 @@
+# GEODIST key member1 member2 [m|km|ft|mi]
+
+GEODIST geopoints Park Bank mi
+GEODIST geopoints Park Bank km
+
+GEODIST geopoints "Junction Road" Park
+GEODIST geopoints "Junction Road" Park m
+GEODIST geopoints "Junction Road" Park km
+GEODIST geopoints "Junction Road" Park mi
+GEODIST geopoints "Junction Road" Park ft
